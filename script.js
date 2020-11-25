ec1dd0e6a1fb1bb169bd96ac640f317c8f4c845c
Robbieb2342 <63669261+Robbieb2342@users.noreply.github.com>
2020-11-25 13:55:14 +0000

    Add files via upload
    Slight problem with slide in menu on mobile view port

---
+++ script.js
@@ -0,0 +1,27 @@
+const navSlide = function() {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  //Toggle Nav
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+          link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
+
+      }
+
+    });
+
+    //Burger animation
+    burger.classList.toggle('toggle');
+  });
+  //Animate links
+
+}
+
+navSlide();
